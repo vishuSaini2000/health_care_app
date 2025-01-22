370260f62363866d6ec71f2b9123539c53c0d770
vishuSaini2000 <sainivishal1235@gmail.com>
2025-01-23 04:14:15 +0530

    consultation category dialog box content done

--- lib/screens/components/consultation_categories.dart
+++ lib/screens/components/consultation_categories.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:health_care_app/constants/color_constants.dart';
 import 'package:health_care_app/constants/path_constants.dart';
 import 'package:health_care_app/provider/app_mode_provider.dart';
+import 'package:health_care_app/screens/components/consultation_categories_item_content.dart';
 import 'package:health_care_app/widgets/common/consultation_categories_item.dart';
 import 'package:health_care_app/widgets/common/consultation_categories_dialog_box.dart';
 
@@ -18,9 +19,10 @@ class _ConsultationCategoriesState
     extends ConsumerState<ConsultationCategories> {
   @override
   Widget build(BuildContext context) {
-    
-    Color appCategoriesCircleAvatarBgColor = ref.watch(appCategoriesCircleAvatarBgColorProvider);
+    Color appCategoriesCircleAvatarBgColor =
+        ref.watch(appCategoriesCircleAvatarBgColorProvider);
     Color appTextColor = ref.watch(appTextColorProvider);
+    Color appContainerColor = ref.watch(appContainerColorProvider);
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Row(
@@ -37,9 +39,13 @@ class _ConsultationCategoriesState
             onTap: () {
               showDialog(
                 context: context,
-                builder: (context) => const ConsultationCategoriesDialogBox(
-                  backgroundColor: appLightBlue,
-                  child: Text('hi'),
+                builder: (context) => ConsultationCategoriesDialogBox(
+                  backgroundColor: appContainerColor,
+                  child: const ConsultationCategoriesItemContent(
+                    barText: 'Consult with Expert Doctors',
+                    mainText:
+                        'Our network of experienced doctors is available to address your health issues through consultations via video, voice, or chat. Get professional medical advice tailored to your symptoms and health records, all from the comfort of your home.',
+                  ),
                 ),
               );
             },
@@ -55,9 +61,12 @@ class _ConsultationCategoriesState
             onTap: () {
               showDialog(
                 context: context,
-                builder: (context) => const ConsultationCategoriesDialogBox(
-                  backgroundColor: appLightBlue,
-                  child: Text('hi'),
+                builder: (context) => ConsultationCategoriesDialogBox(
+                  backgroundColor: appContainerColor,
+                  child: const ConsultationCategoriesItemContent(
+                    barText: 'Hospital Services ',
+                    mainText: 'Access a wide range of hospital services, including emergency care, routine check-ups, and specialized treatments. Use our app to find hospitals based on specialty, availability, and location. Book and manage your appointments with ease.',
+                  ),
                 ),
               );
             },
@@ -73,9 +82,11 @@ class _ConsultationCategoriesState
             onTap: () {
               showDialog(
                 context: context,
-                builder: (context) => const ConsultationCategoriesDialogBox(
-                  backgroundColor: appLightBlue,
-                  child: Text('hi'),
+                builder: (context) => ConsultationCategoriesDialogBox(
+                  backgroundColor: appContainerColor,
+                  child: const ConsultationCategoriesItemContent(
+                    barText: "Healthcare Products",mainText: 'Browse our extensive catalog of healthcare products, including personal care items, fitness supplements, and prescription medications. Enjoy seamless online shopping with home delivery options.',
+                  ),
                 ),
               );
             },
@@ -91,9 +102,12 @@ class _ConsultationCategoriesState
             onTap: () {
               showDialog(
                 context: context,
-                builder: (context) => const ConsultationCategoriesDialogBox(
-                  backgroundColor: appLightBlue,
-                  child: Text('hi'),
+                builder: (context) => ConsultationCategoriesDialogBox(
+                  backgroundColor: appContainerColor,
+                  child: const ConsultationCategoriesItemContent(
+                    barText: "Customer Care and Assistance",
+                    mainText: 'Need assistance with your account, medical services, or billing? Our dedicated support team is here to ensure your experience is hassle-free. Reach out for immediate assistance or to provide feedback about our services.',
+                  ),
                 ),
               );
             },
--- lib/widgets/common/app_text_widget.dart
+++ lib/widgets/common/app_text_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:health_care_app/constants/color_constants.dart';
 
 class AppTextWidget extends StatefulWidget {
+  final TextAlign? textAlign;
   final String text;
   final TextDecoration? textDecoration;
   final double fontSize;
@@ -10,6 +11,7 @@ class AppTextWidget extends StatefulWidget {
 
   const AppTextWidget({
     super.key,
+    this.textAlign,
     required this.text,
     this.textDecoration,
     this.fontSize = 10.0,
@@ -24,6 +26,7 @@ class _AppTextWidgetState extends State<AppTextWidget> {
   @override
   Widget build(BuildContext context) {
     return Text(
+      textAlign: widget.textAlign,
       widget.text,
       style: TextStyle(
         decoration: widget.textDecoration,
